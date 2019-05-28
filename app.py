cd4a446f62c43dde67344a9a59e82b48a8f583ee
KingdevNL <jasper@kingdev.nl>
2019-05-28 13:08:56 +0200

    fix: blank homepage

--- app.py
+++ app.py
@@ -6,12 +6,13 @@ PORT_NUMBER = 8080
 
 class MyHandler(BaseHTTPRequestHandler):
 
-  def do_GET(self):
-    """Handler for GET requests"""
-    self.send_response(200)
-    self.send_header('Content-type','text/html')
-    self.end_headers()
-    self.wfile.write("<h1>Hello to this app</h1>")
+	def do_GET(self):
+		self.send_response(200)
+		self.send_header('Content-type','text/html')
+		self.end_headers()
+		# Send the html message
+		self.wfile.write("Hello World !")
+		return
 
 try:
   server = HTTPServer(('', PORT_NUMBER), MyHandler)
