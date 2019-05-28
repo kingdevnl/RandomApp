0a0d4289754a86a7d32771a7dd1d321c64772d25
KingdevNL <jasper@kingdev.nl>
2019-05-28 12:59:07 +0200

    Update index page

--- app.py
+++ app.py
@@ -9,10 +9,9 @@ class MyHandler(BaseHTTPRequestHandler):
   def do_GET(self):
     """Handler for GET requests"""
     self.send_response(200)
-    self.send_header('Content-type','image/png')
+    self.send_header('Content-type','text/html')
     self.end_headers()
-    with open('logo.png', 'rb') as f:
-      self.wfile.write(f.read())
+    self.wfile.write("<h1>Hello to this app</h1>")
 
 try:
   server = HTTPServer(('', PORT_NUMBER), MyHandler)
